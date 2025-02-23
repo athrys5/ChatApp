191bae8aa05b7633133474c41db2b15b39aae094
athrys5 <capriotti.andrea00@gmail.com>
2025-02-23 10:45:30 +0100

    Improved chat ui

--- chat-ui/src/components/MessageContainer.tsx
+++ chat-ui/src/components/MessageContainer.tsx
@@ -1,4 +1,4 @@
-import { Box, useTheme } from "@mui/material";
+import { Box, Grid2, useTheme } from "@mui/material";
 import { IMessage } from "../interfaces/GenericInterfaces";
 
 interface IMessageContainerProps {
@@ -11,27 +11,60 @@ const MessageContainer: React.FC<IMessageContainerProps> = ({
   username,
 }) => {
   const theme = useTheme();
+
   return (
     <Box>
       {messages.map((msg, index) => {
+        const showIconAndUsername =
+          index === 0 || messages[index - 1].username !== msg.username;
+
         return (
-          <Box
-            key={index}
-            sx={{
-              display: "flex",
-              flex: 1,
-              backgroundColor: theme.palette.primary.main,
-              height: "3rem",
-              borderRadius: "1.5rem",
-              padding: "10px",
-              margin: "10px",
-              minWidth: "15rem",
-              maxWidth: "30rem",
-              justifySelf:
-                username === msg.username ? "flex-end" : "flex-start",
-            }}
-          >
-            {msg.msg} - {msg.username}
+          <Box key={index}>
+            {index > 0 && messages[index - 1].username !== msg.username ? (
+              <Box
+                sx={{
+                  height: "1px",
+                  backgroundColor: theme.palette.secondary.main,
+                  margin: "8px",
+                }}
+              ></Box>
+            ) : null}
+
+            <Grid2 container spacing={0}>
+              <Grid2 size={1}>
+                {showIconAndUsername && (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        width: "2.5rem",
+                        height: "2.5rem",
+                        borderRadius: "50%",
+
+                        backgroundColor: theme.palette.secondary.main,
+                      }}
+                    />
+                  </Box>
+                )}
+              </Grid2>
+              <Grid2 size={11}>
+                {showIconAndUsername && (
+                  <Box sx={{ fontWeight: "bold" }}>{msg.username}</Box>
+                )}
+                <Box
+                  sx={{
+                    wordWrap: "break-word",
+                    overflowWrap: "break-word",
+                  }}
+                >
+                  {msg.msg}
+                </Box>
+              </Grid2>
+            </Grid2>
           </Box>
         );
       })}
